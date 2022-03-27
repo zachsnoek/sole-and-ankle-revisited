54c927293698e5da9efb59e6e2111fa7615b8e1c
zachsnoek <dev@zachsnoek.com>
2022-03-27 09:15:29 -0700

    Add breakpoints.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,20 +5,20 @@ import Header from '../Header';
 import ShoeIndex from '../ShoeIndex';
 
 const App = () => {
-  const [sortId, setSortId] = React.useState('newest');
+    const [sortId, setSortId] = React.useState('newest');
 
-  return (
-    <>
-      <Header />
-      <Main>
-        <ShoeIndex sortId={sortId} setSortId={setSortId} />
-      </Main>
-    </>
-  );
+    return (
+        <>
+            <Header />
+            <Main>
+                <ShoeIndex sortId={sortId} setSortId={setSortId} />
+            </Main>
+        </>
+    );
 };
 
 const Main = styled.main`
-  padding: 64px 32px;
+    padding: 64px 32px;
 `;
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ThemeProvider } from 'styled-components';
 
 import App from './components/App';
 import GlobalStyles from './components/GlobalStyles';
 
+import { theme } from './constants';
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-    <GlobalStyles />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <ThemeProvider theme={theme}>
+            <App />
+            <GlobalStyles />
+        </ThemeProvider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
