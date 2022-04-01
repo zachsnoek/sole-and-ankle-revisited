6a1c30b24fc14903f3f62d140187b6389931bac2
zachsnoek <dev@zachsnoek.com>
2022-03-31 20:49:15 -0700

    My work for exercise 2.

--- src/components/Icon/Icon.js
+++ src/components/Icon/Icon.js
@@ -1,41 +1,35 @@
 import React from 'react';
 import styled from 'styled-components/macro';
-import {
-  Search,
-  Menu,
-  ShoppingBag,
-  ChevronDown,
-  X,
-} from 'react-feather';
+import { Search, Menu, ShoppingBag, ChevronDown, X } from 'react-feather';
 
 const icons = {
-  search: Search,
-  menu: Menu,
-  'shopping-bag': ShoppingBag,
-  'chevron-down': ChevronDown,
-  close: X,
+    search: Search,
+    menu: Menu,
+    'shopping-bag': ShoppingBag,
+    'chevron-down': ChevronDown,
+    close: X,
 };
 
 const Icon = ({ id, color, size, strokeWidth, ...delegated }) => {
-  const Component = icons[id];
+    const Component = icons[id];
 
-  if (!Component) {
-    throw new Error(`No icon found for ID: ${id}`);
-  }
+    if (!Component) {
+        throw new Error(`No icon found for ID: ${id}`);
+    }
 
-  return (
-    <Wrapper strokeWidth={strokeWidth} {...delegated}>
-      <Component color={color} size={size} />
-    </Wrapper>
-  );
+    return (
+        <Wrapper strokeWidth={strokeWidth} {...delegated}>
+            <Component color={color} size={size} />
+        </Wrapper>
+    );
 };
 
 const Wrapper = styled.div`
-  & > svg {
-    display: block;
-    stroke-width: ${(p) =>
-      p.strokeWidth !== undefined ? p.strokeWidth + 'px' : undefined};
-  }
+    & > svg {
+        display: block;
+        stroke-width: ${(p) =>
+            p.strokeWidth !== undefined ? p.strokeWidth + 'px' : undefined};
+    }
 `;
 
 export default Icon;
