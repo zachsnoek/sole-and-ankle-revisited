171846833affd87463bbef46ff43541629bb4ffd
zachsnoek <dev@zachsnoek.com>
2022-04-02 10:05:59 -0700

    Exercise 5 solution.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -57,7 +57,6 @@ const MainHeader = styled.div`
     display: flex;
     align-items: baseline;
     padding: 18px 32px;
-    height: 72px;
     border-bottom: 1px solid ${COLORS.gray[300]};
 
     overflow-x: auto;
