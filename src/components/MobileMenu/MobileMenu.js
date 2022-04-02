0aef14ec4aee880a8efa54becc89e398df575e73
zachsnoek <dev@zachsnoek.com>
2022-04-02 09:35:17 -0700

    Solution.

--- src/components/MobileMenu/MobileMenu.js
+++ src/components/MobileMenu/MobileMenu.js
@@ -36,8 +36,10 @@ const MobileMenu = ({ isOpen, onDismiss }) => {
 const ModalOverlay = styled(DialogOverlay)`
     position: fixed;
     top: 0;
-    height: 100%;
-    width: 100%;
+    right: 0;
+    bottom: 0;
+    left: 0;
+
     background: hsla(220, 5%, 40%, 0.8);
 `;
 
